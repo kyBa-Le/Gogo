c777a04133e38c04961d1c8bedce95d2d3e0596d
KaPhuDong <dong.ka26@student.passerellesnumeriques.org>
2024-11-25 20:35:11 +0700

    14 | culture-page

---
+++ public/view/components/card.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/main.css">
+</head>
+<body>
+    <main>
+        <div class="swiper-slide card-cultural">
+            <div class="card-img">
+                <img src="https://ohayo-onsen.com/wp-content/uploads/2023/07/du-lich-ha-noi-1-ngay-nen-di-dau-1.jpg" alt="" class="cultural-img">
+            </div>
+            <div class="content">
+                <h5 class="title">Hà Nội</h5>
+                <div class="description">Lorem ipsum dolor sit, amet consectetur adipisicing.</div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ public/view/components/heading.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heading</title>
+    <link rel="stylesheet" href="../../css/main.css">
+</head>
+<body>
+    <main>
+        <div class="heading">
+            <h1 class="heading-title">Make your holiday Memorable</h1>
+            <p class="heading-description">
+                Travor is one of the most popular Travel agency for those who want to explore the wold and try to make adventure
+            </p>
+        </div>
+
+        <br>
+
+        <div class="heading heading-section">
+            <h1 class="heading-title">Best culture</h1>
+            <p class="heading-description">Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+
+    </main>
+    
+</body>
+</html>
